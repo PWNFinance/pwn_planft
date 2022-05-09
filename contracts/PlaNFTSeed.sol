8d05c1f3e4616df210d913905458f7e66d4b1aa0
ashhanai <ashhab.naim@gmail.com>
2022-05-09 13:25:26 +0200

    Remove unnecessary interface

--- contracts/PlaNFTSeed.sol
+++ contracts/PlaNFTSeed.sol
@@ -4,15 +4,9 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IPlaNFTSeed {
-	function mint(address owner, uint256 amount) external;
-}
+contract PlaNFTSeed is ERC20("PlaNFTSeed", "PNFTS"), Ownable {
 
-contract PlaNFTSeed is ERC20("PlaNFTSeed", "PNFTS"), Ownable, IPlaNFTSeed {
-
-	// TODO: Metadata
-
-	function mint(address owner, uint256 amount) override external onlyOwner {
+	function mint(address owner, uint256 amount) external onlyOwner {
 		_mint(owner, amount);
 	}
 
